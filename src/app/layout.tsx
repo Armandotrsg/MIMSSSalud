01a65cb91b990a3a1af0c3f6c5f597f670e86318
Armando Terrazas Gómez <A01640924@tec.mx>
2024-05-18 13:33:27 -0600

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "@/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+
+import { TRPCReactProvider } from "@/trpc/react";
+import { Providers } from "./providers";
+
+export const metadata = {
+  title: "Mi IMSS Salud",
+  description: "Mi IMSS Salud",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es" className={`${GeistSans.variable}`}>
+      <body>
+        <TRPCReactProvider>
+          <Providers>{children}</Providers>
+        </TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
